b5da34fa78570e7418fbbfc052c2da13a65071b8
Luis Doubrava <luis@cg.nl>
2012-09-17 14:28:48 +0200

    add Facade class

---
+++ lib/cg_facade/facade.rb
@@ -0,0 +1,27 @@
+module CgFacade
+  class Facade
+    cattr_accessor :loader_facades
+
+    class << self
+      def register_facade(klass)
+        self.loader_facades ||= []
+        self.loader_facades.delete_if{|f| f.to_s == klass.to_s}
+        self.loader_facades << klass
+      end
+
+      def find_modules_by_name(facade)
+        modules = []
+        find_facades_by_name(facade).each do |f|
+          modules << f.to_s.split("::").first
+        end
+        modules.uniq
+      end
+
+      def find_facades_by_name(facade)
+        self.loader_facades.select{|f| f.to_s.split("::").last == facade}
+      end
+    end
+
+
+  end
+end
