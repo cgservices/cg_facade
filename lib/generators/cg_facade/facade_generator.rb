5ac3291bd34fe9d32fcd739a016e727c3e79623e
Luis Doubrava <luis@cg.nl>
2012-09-17 14:28:15 +0200

    add source_root

--- lib/generators/cg_facade/facade_generator.rb
+++ lib/generators/cg_facade/facade_generator.rb
@@ -5,6 +5,10 @@ module CgFacade
     class FacadeGenerator < Rails::Generators::NamedBase 
       argument :actions, :type => :array, :default => [], :banner => "action action"
 
+      def self.source_root
+        @source_root ||= File.expand_path('../templates', __FILE__)
+      end
+      
       def create_facade_files
         template 'facade.rb', File.join('app/facade', class_path, "#{file_name}_facade.rb")
       end
