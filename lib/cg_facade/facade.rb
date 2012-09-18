c6c6a0e706dd636077011be3c063397afe5902c5
Luis Doubrava <luis@cg.nl>
2012-09-18 09:42:40 +0200

    cleanup code

--- lib/cg_facade/facade.rb
+++ lib/cg_facade/facade.rb
@@ -3,12 +3,14 @@ module CgFacade
     cattr_accessor :loader_facades
 
     class << self
+      # Add klass to loader facade
       def register_facade(klass)
         self.loader_facades ||= []
         self.loader_facades.delete_if{|f| f.to_s == klass.to_s}
         self.loader_facades << klass
       end
 
+      # Return the name of the modules that implement facade
       def find_modules_by_name(facade)
         modules = []
         find_facades_by_name(facade).each do |f|
